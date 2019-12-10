21f7eafe5879c364b4108564894816bccf8230dc
Dakojr <fri.develop1@gmail.com>
2019-12-10 16:27:01 +0100

    rendu monnaie

---
+++ exo.php
@@ -0,0 +1,47 @@
+<?php
+
+function renduMonnaie($total)
+{
+    $totaltmp = $total;
+    for ($i = 0; $total >= 20; $i++) {
+        $total = $total - 20;
+    }
+
+    if ($i > 0) {
+        echo $i . " billets de 20 ont été rendu <br>";
+    }
+    for ($i = 0; $total >= 10; $i++) {
+        $total = $total - 10;
+    }
+    if ($i > 0) {
+        echo $i . " billets de 10 ont été rendu <br>";
+    }
+    for ($i = 0; $total >= 5; $i++) {
+        $total = $total - 5;
+    }
+    if ($i > 0) {
+        echo $i . " billets de 5 ont été rendu <br>";
+    }
+    for ($i = 0; $total >= 2; $i++) {
+        $total = $total - 2;
+    }
+    if ($i > 0) {
+        echo $i . " pièces de 2 ont été rendu <br>";
+    }
+    for ($i = 0; $total >= 1; $i++) {
+        $total = $total - 1;
+    }
+    if ($i > 0) {
+        echo $i . " pièces de 1 ont été rendu <br>";
+    }
+
+    if ($total > 0) {
+        echo "il reste " . $total . " à rendre";
+    }
+
+    echo "Pour un total de " . $totaltmp;
+
+    return $totaltmp;
+}
+
+renduMonnaie(13);
